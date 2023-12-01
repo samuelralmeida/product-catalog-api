523266e9e17591de702c47bfd8900d09bbee3a31
Samuel Almeida <samuel.sousa@farme.com.br>
2023-11-30 23:32:41 -0300

    installing chi router

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/samuelralmeida/product-catalog-api
 
 go 1.21.1
+
+require github.com/go-chi/chi/v5 v5.0.10
--- main.go
+++ main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Product Catalog API</h1>")
 }
 
 func main() {
-	http.HandleFunc("/", handleFunc)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Get("/", homeHandler)
+
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) { http.Error(w, "Page not found", http.StatusNotFound) })
+
 	fmt.Println("Starting the server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", r)
 }
