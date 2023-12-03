42eb9e471c901db9faee219aef28b3ffafb7069c
Samuel Almeida <samuel.sousa@farme.com.br>
2023-12-03 17:32:12 -0300

    create view package of MVC structure

--- main.go
+++ main.go
@@ -2,31 +2,23 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"log"
 	"net/http"
 	"path/filepath"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/samuelralmeida/product-catalog-api/views"
 )
 
 func executeTemplate(w http.ResponseWriter, filepath string) {
-	w.Header().Set("Context-Type", "text/html; charset=utf-8")
-
-	tpl, err := template.ParseFiles(filepath)
+	viewTemplate, err := views.Parse(filepath)
 	if err != nil {
 		log.Printf("parsing template: %v", err)
 		http.Error(w, "there was an error parsing the template", http.StatusInternalServerError)
 		return
 	}
-
-	err = tpl.Execute(w, nil)
-	if err != nil {
-		log.Printf("executing template: %v", err)
-		http.Error(w, "there was an error executing the template", http.StatusInternalServerError)
-		return
-	}
+	viewTemplate.Execute(w, nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
---
+++ views/template.go
@@ -0,0 +1,30 @@
+package views
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func Parse(filepath string) (Template, error) {
+	tpl, err := template.ParseFiles(filepath)
+	if err != nil {
+		return Template{}, fmt.Errorf("parsing template: %w", err)
+	}
+	return Template{HtmlTemplate: tpl}, nil
+}
+
+type Template struct {
+	HtmlTemplate *template.Template
+}
+
+func (t Template) Execute(w http.ResponseWriter, data interface{}) {
+	w.Header().Set("Context-Type", "text/html; charset=utf-8")
+	err := t.HtmlTemplate.Execute(w, data)
+	if err != nil {
+		log.Printf("executing template: %v", err)
+		http.Error(w, "there was an error executing the template", http.StatusInternalServerError)
+		return
+	}
+}
